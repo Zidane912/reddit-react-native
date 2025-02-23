f803b3414af39c29abf08b46e0c4016e23015f2d
Zidane912 <alam.zidane912@gmail.com>
2025-02-23 00:48:28 +0000

    now using livewire project for api call

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.102/api', // your API URL
+  baseURL: 'https://redditcloneapp.co.uk/api', // your API URL
 });
 
 // Attach token to every request
